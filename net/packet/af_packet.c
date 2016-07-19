a4dc1407962921f0094936d77b794bb6de11c515
Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
2016-07-19 14:40:51 +0900

    packet: fix second argument of sock_tx_timestamp()
    This patch fixes an issue that a syscall (e.g. sendto syscall) cannot
    work correctly. Since the sendto syscall doesn't have msg_control buffer,
    the sock_tx_timestamp() in packet_snd() cannot work correctly because
    the socks.tsflags is set to 0.
    So, this patch sets the socks.tsflags to sk->sk_tsflags as default.
    
    Fixes: c14ac9451c34 ("sock: enable timestamping using control messages")
    Reported-by: Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>
    Reported-by: Keita Kobayashi <keita.kobayashi.ym@renesas.com>
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Acked-by: Soheil Hassas Yeganeh <soheil@google.com>
    Acked-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -1951,7 +1951,7 @@ retry:
 		goto out_unlock;
 	}
 
-	sockc.tsflags = 0;
+	sockc.tsflags = sk->sk_tsflags;
 	if (msg->msg_controllen) {
 		err = sock_cmsg_send(sk, msg, &sockc);
 		if (unlikely(err)) {
@@ -2546,7 +2546,7 @@ static int tpacket_snd(struct packet_sock *po, struct msghdr *msg)
 		}
 	}
 
-	sockc.tsflags = 0;
+	sockc.tsflags = po->sk.sk_tsflags;
 	if (msg->msg_controllen) {
 		err = sock_cmsg_send(&po->sk, msg, &sockc);
 		if (unlikely(err))
@@ -2731,7 +2731,7 @@ static int packet_snd(struct socket *sock, struct msghdr *msg, size_t len)
 	if (unlikely(!(dev->flags & IFF_UP)))
 		goto out_unlock;
 
-	sockc.tsflags = 0;
+	sockc.tsflags = sk->sk_tsflags;
 	sockc.mark = sk->sk_mark;
 	if (msg->msg_controllen) {
 		err = sock_cmsg_send(sk, msg, &sockc);

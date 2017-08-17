53bdea066cd5e9d2d8104e48888a1134eab77bb0
Ajay Agarwal <ajaya@codeaurora.org>
2017-08-17 18:47:19 +0530

    usb: dwc3-msm: Allow PM suspend in host mode irrespective of runtimePM
    DWC3 driver uses wakeup_source and allows pm_suspend only after
    entering low power mode as part of runtime suspend. This prevents
    PM suspend in host mode if connected device's driver doesn't
    support runtime or selective suspend.
    Add support in driver where user can specify using dtsi attribute
    to not use wakeup source in host mode. It will allow system to enter
    deep sleep or pm_suspend irrespective of runtime PM state of XHCI.
    On high level below are the changes in default behavior:
     -Do not use wakeup_source for DWC3 when operating in host mode.
     -For host mode, devices will suspend upon PM suspend and resumed
      after PM resume.
    
    Change-Id: Ia1d450e6dd99e094e48caf9723a355b7870b54dd
    Signed-off-by: Ajay Agarwal <ajaya@codeaurora.org>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -948,7 +948,7 @@ int xhci_suspend(struct xhci_hcd *xhci, bool do_wakeup)
 	struct usb_hcd		*hcd = xhci_to_hcd(xhci);
 	u32			command;
 
-	if (!hcd->state)
+	if (!hcd->state || xhci->suspended)
 		return 0;
 
 	if (hcd->state != HC_STATE_SUSPENDED ||
@@ -1018,6 +1018,7 @@ int xhci_suspend(struct xhci_hcd *xhci, bool do_wakeup)
 	/* step 5: remove core well power */
 	/* synchronize irq when using MSI-X */
 	xhci_msix_sync_irqs(xhci);
+	xhci->suspended = true;
 
 	return rc;
 }
@@ -1038,7 +1039,7 @@ int xhci_resume(struct xhci_hcd *xhci, bool hibernated)
 	bool			comp_timer_running = false;
 	bool			pending_portevent = false;
 
-	if (!hcd->state)
+	if (!hcd->state || !xhci->suspended)
 		return 0;
 
 	/* Wait a bit if either of the roothubs need to settle from the
@@ -1197,6 +1198,7 @@ int xhci_resume(struct xhci_hcd *xhci, bool hibernated)
 
 	/* Re-enable port polling. */
 	xhci_dbg(xhci, "%s: starting port polling.\n", __func__);
+	xhci->suspended = false;
 	set_bit(HCD_FLAG_POLL_RH, &xhci->shared_hcd->flags);
 	usb_hcd_poll_rh_status(xhci->shared_hcd);
 	set_bit(HCD_FLAG_POLL_RH, &hcd->flags);

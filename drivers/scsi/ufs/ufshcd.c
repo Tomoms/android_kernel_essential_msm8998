159db53be4041f28900d14556b48ea4c6fc8b7a0
Jaegeuk Kim <jaegeuk@google.com>
2018-06-05 18:42:19 -0700

    scsi: ufs: disable interrupt in clk-gating
    This potentially fixes noc error caused by irq disabled.
    
    Bug: 79295949
    Change-Id: I04a96dd1ab483a1928c6b3170579200c7ab2f512
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -1506,6 +1506,8 @@ static void ufshcd_ungate_work(struct work_struct *work)
 	ufshcd_hba_vreg_set_hpm(hba);
 	ufshcd_enable_clocks(hba);
 
+	ufshcd_enable_irq(hba);
+
 	/* Exit from hibern8 */
 	if (ufshcd_can_hibern8_during_gating(hba)) {
 		/* Prevent gating in this path */
@@ -1670,6 +1672,8 @@ static void ufshcd_gate_work(struct work_struct *work)
 		}
 		ufshcd_set_link_hibern8(hba);
 	}
+
+	ufshcd_disable_irq(hba);
 
 	/*
 	 * If auto hibern8 is supported then the link will already

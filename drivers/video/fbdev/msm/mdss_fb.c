8b17863fd940dd56c2d385948565a011c0ccaa73
Tommaso Fonda <fondatommaso2@gmail.com>
2021-12-11 16:30:33 +0100

    mdss: restore original x offset
    	* To reflect the actual resolution of the screen
    	  along the x axis, which is 1312 pixels.
    
    	* Before:
    	  $ adb shell wm size
    	  > Physical size: 1316x2560
    
    	  After:
    	  $ adb shell wm size
    	  > Physical size: 1312x2560

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -398,7 +398,7 @@ static int mdss_fb_get_panel_xres(struct mdss_panel_info *pinfo)
 	if (pinfo->split_link_enabled)
 		xres = xres * pinfo->mipi.num_of_sublinks;
 #ifdef CONFIG_BOARD_MATA
-	return xres - 124;
+	return xres - 128;
 #else
 	return xres;
 #endif

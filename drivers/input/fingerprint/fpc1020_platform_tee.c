f202d652ab3b62f21e6429e1ba8a6cade6204d97
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-10-19 18:49:54 +0200

    fingerprint: fpc: Inline clk_enable_store() to return count
    Calling a function just to represent the size of object
    using size_t and making the assigned variable return
    is just nonsense.
    
    Change-Id: I34965a338dad4a1005ef0bba92fc43094e959a65

--- drivers/input/fingerprint/fpc1020_platform_tee.c
+++ drivers/input/fingerprint/fpc1020_platform_tee.c
@@ -75,7 +75,7 @@ struct fpc1020_data {
  * backwards compatibility. Only prints a debug print that it is
  * disabled.
  */
-static ssize_t clk_enable_set(struct device *dev,
+static inline ssize_t clk_enable_set(struct device *dev,
 	struct device_attribute *attr,
 	const char *buf, size_t count)
 {

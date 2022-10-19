273465d26244b9970e55e82f447fd45b2b897e30
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-10-19 18:39:14 +0200

    fingerprint: fpc: Don't measure latency
    This writes to the irq node to drop a printk message
    to measure latency. But we won't do anything with that
    information. Simply nuke the whole function.
    
    Change-Id: I21d0f285aade34ebaa1ec2461b95472a116ec81e

--- drivers/input/fingerprint/fpc1020_platform_tee.c
+++ drivers/input/fingerprint/fpc1020_platform_tee.c
@@ -221,21 +221,7 @@ static ssize_t irq_get(struct device *dev,
 	return scnprintf(buf, PAGE_SIZE, "%i\n", irq);
 }
 
-/**
- * writing to the irq node will just drop a printk message
- * and return success, used for latency measurement.
- */
-static ssize_t irq_ack(struct device *dev,
-	struct device_attribute *attr,
-	const char *buf, size_t count)
-{
-	struct fpc1020_data *fpc1020 = dev_get_drvdata(dev);
-
-	dev_dbg(fpc1020->dev, "%s\n", __func__);
-
-	return count;
-}
-static DEVICE_ATTR(irq, S_IRUSR | S_IWUSR, irq_get, irq_ack);
+static DEVICE_ATTR(irq, S_IRUSR | S_IWUSR, irq_get);
 
 static struct attribute *attributes[] = {
 	&dev_attr_pinctl_set.attr,

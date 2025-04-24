2be8d28cf3eb33df0df85cc6cc597b40c4b111db
Tommaso Fonda <fondatommaso2@gmail.com>
2025-04-24 12:51:38 +0200

    Merge tag 'v4.4-st63' of https://git.kernel.org/pub/scm/linux/kernel/git/cip/linux-cip into v
    version 4.4-st63
    
    Change-Id: I201129ce660e5a8f74551b06e8009092d999d503

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -896,10 +896,11 @@ static int set_config(struct usb_composite_dev *cdev,
 	}
 
 done:
-	if (USB_VBUS_DRAW(gadget->speed) <= USB_SELF_POWER_VBUS_MAX_DRAW)
-		usb_gadget_set_selfpowered(gadget);
-	else
+	if (USB_VBUS_DRAW(gadget->speed) > USB_SELF_POWER_VBUS_MAX_DRAW ||
+	    (c && !(c->bmAttributes & USB_CONFIG_ATT_SELFPOWER)))
 		usb_gadget_clear_selfpowered(gadget);
+	else
+		usb_gadget_set_selfpowered(gadget);
 
 	usb_gadget_vbus_draw(gadget, USB_VBUS_DRAW(gadget->speed));
 	if (result >= 0 && cdev->delayed_status)
@@ -2394,7 +2395,10 @@ void composite_suspend(struct usb_gadget *gadget)
 	cdev->suspended = 1;
 	spin_unlock_irqrestore(&cdev->lock, flags);
 
-	usb_gadget_set_selfpowered(gadget);
+	if (cdev->config &&
+	    cdev->config->bmAttributes & USB_CONFIG_ATT_SELFPOWER)
+		usb_gadget_set_selfpowered(gadget);
+
 	usb_gadget_vbus_draw(gadget, 2);
 }
 
@@ -2434,8 +2438,11 @@ void composite_resume(struct usb_gadget *gadget)
 				f->resume(f);
 		}
 
-		if (USB_VBUS_DRAW(gadget->speed) > USB_SELF_POWER_VBUS_MAX_DRAW)
+		if (USB_VBUS_DRAW(gadget->speed) > USB_SELF_POWER_VBUS_MAX_DRAW ||
+		    !(cdev->config->bmAttributes & USB_CONFIG_ATT_SELFPOWER))
 			usb_gadget_clear_selfpowered(gadget);
+		else
+			usb_gadget_set_selfpowered(gadget);
 
 		usb_gadget_vbus_draw(gadget, USB_VBUS_DRAW(gadget->speed));
 	}

08b19f6fa46f88ee3493544eab0fb12df0d9b7e1
Nanhumly <admin@warbler.cc>
2025-07-15 13:18:12 +0000

    fixup! bpf: add writable context for raw tracepoints
    Change-Id: I7a2d19f3b8c01e45f6d82b0e3714a5c8f9b63e22

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -274,9 +274,9 @@ enum bpf_reg_type {
 	PTR_TO_SOCK_COMMON_OR_NULL, /* reg points to sock_common or NULL */
 	PTR_TO_TCP_SOCK,	 /* reg points to struct tcp_sock */
 	PTR_TO_TCP_SOCK_OR_NULL, /* reg points to struct tcp_sock or NULL */
+	PTR_TO_TP_BUFFER,	 /* reg points to a writable raw tp's buffer */
 	PTR_TO_MEM,		 /* reg points to valid memory region */
 	PTR_TO_MEM_OR_NULL,	 /* reg points to valid memory region or NULL */
-	PTR_TO_TP_BUFFER,	 /* reg points to a writable raw tp's buffer */
 };
 
 /* The information passed from prog-specific *_is_valid_access

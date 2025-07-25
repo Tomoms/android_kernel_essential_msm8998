61626e90e2f9110641d7ba0f25b4225b55a076fe
darkhz <kmachanwenw@gmail.com>
2025-07-25 11:55:15 +0200

    mm: Do not reserve memory for users
    Waste of memory.
    
    Change-Id: I0f6388724a9dab62caf1a2525b5611913263cc7f

--- mm/nommu.c
+++ mm/nommu.c
@@ -54,7 +54,7 @@ int sysctl_overcommit_ratio = 50; /* default is 50% */
 unsigned long sysctl_overcommit_kbytes __read_mostly;
 int sysctl_max_map_count = DEFAULT_MAX_MAP_COUNT;
 int sysctl_nr_trim_pages = CONFIG_NOMMU_INITIAL_TRIM_EXCESS;
-unsigned long sysctl_user_reserve_kbytes __read_mostly = 1UL << 17; /* 128MB */
+unsigned long sysctl_user_reserve_kbytes __read_mostly = 0; /* 0MB */
 unsigned long sysctl_admin_reserve_kbytes __read_mostly = 1UL << 13; /* 8MB */
 int heap_stack_gap = 0;
 
@@ -2084,11 +2084,6 @@ int nommu_shrink_inode_mappings(struct inode *inode, size_t size,
  */
 static int __meminit init_user_reserve(void)
 {
-	unsigned long free_kbytes;
-
-	free_kbytes = global_page_state(NR_FREE_PAGES) << (PAGE_SHIFT - 10);
-
-	sysctl_user_reserve_kbytes = min(free_kbytes / 32, 1UL << 17);
 	return 0;
 }
 subsys_initcall(init_user_reserve);

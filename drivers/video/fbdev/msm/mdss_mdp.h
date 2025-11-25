18b23d6cabc1b1d1594c23ef0e182064609f18c8
Tommaso Fonda <fondatommaso2@gmail.com>
2025-11-25 10:58:25 +0100

    drivers: video: msm: Guard redefinition of BITS_TO_BYTES
    Change-Id: If5ccea22f092027b3b0a9729bac3d097474b0dfb

--- drivers/video/fbdev/msm/mdss_mdp.h
+++ drivers/video/fbdev/msm/mdss_mdp.h
@@ -140,7 +140,9 @@
 #define HW_CURSOR_STAGE(mdata) \
 	(((mdata)->max_target_zorder + MDSS_MDP_STAGE_0) - 1)
 
+#ifndef BITS_TO_BYTES
 #define BITS_TO_BYTES(x) DIV_ROUND_UP(x, BITS_PER_BYTE)
+#endif
 
 #define PP_PROGRAM_PA		0x1
 #define PP_PROGRAM_PCC		0x2

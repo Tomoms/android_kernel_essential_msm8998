17384caa702dbe47d4a0020bd46f06722d4f17c4
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2026-04-30 15:46:54 -0400

    thermal: core: Fix thermal zone governor cleanup issues
    [ Upstream commit 41ff66baf81c6541f4f985dd7eac4494d03d9440 ]
    
    If thermal_zone_device_register_with_trips() fails after adding
    a thermal governor to the thermal zone being registered, the
    governor is not removed from it as appropriate which may lead to
    a memory leak.
    
    In turn, thermal_zone_device_unregister() calls thermal_set_governor()
    without acquiring the thermal zone lock beforehand which may race with
    a governor update via sysfs and may lead to a use-after-free in that
    case.
    
    Address these issues by adding two thermal_set_governor() calls, one to
    thermal_release() to remove the governor from the given thermal zone,
    and one to the thermal zone registration error path to cover failures
    preceding the thermal zone device registration.
    
    Fixes: e33df1d2f3a0 ("thermal: let governors have private data for each thermal zone")
    Cc: All applicable <stable@vger.kernel.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://patch.msgid.link/5092923.31r3eYUQgx@rafael.j.wysocki
    [ adapted context for missing mutex_destroy/complete ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [uli: backport to 4.4]
    Signed-off-by: Ulrich Hecht <uli@kernel.org>
    Reviewed-by: Pavel Machek <pavel@nabladev.com>
    Reviewed-by: Nobuhiro Iwamatsu <nobuhiro.iwamatsu.x90@mail.toshiba>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1886,6 +1886,7 @@ static void thermal_release(struct device *dev)
 	if (!strncmp(dev_name(dev), "thermal_zone",
 		     sizeof("thermal_zone") - 1)) {
 		tz = to_thermal_zone(dev);
+		thermal_set_governor(tz, NULL);
 		kfree(tz);
 	} else if(!strncmp(dev_name(dev), "cooling_device",
 			sizeof("cooling_device") - 1)){
@@ -2480,7 +2481,6 @@ void thermal_zone_device_unregister(struct thermal_zone_device *tz)
 	device_remove_file(&tz->device, &dev_attr_policy);
 	device_remove_file(&tz->device, &dev_attr_available_policies);
 	remove_trip_attrs(tz);
-	thermal_set_governor(tz, NULL);
 
 	thermal_remove_hwmon_sysfs(tz);
 	flush_work(&tz->sensor.work);

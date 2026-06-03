6af4425953995a99435aa960ec68109c98514243
Tommaso Fonda <fondatommaso2@gmail.com>
2026-06-03 15:49:39 +0200

    Revert "enable printk"
    This reverts commit 2d8a6aa521e3f00443d1b35169d97117af0698fc.

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -59,7 +59,7 @@
 extern void printascii(char *);
 #endif
 
-static __read_mostly bool enabled = 1;
+static __read_mostly bool enabled;
 module_param(enabled, bool, S_IRUGO | S_IWUSR);
 
 int console_printk[4] = {
